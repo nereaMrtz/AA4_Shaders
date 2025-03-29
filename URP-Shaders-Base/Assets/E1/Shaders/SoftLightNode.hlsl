70360cb0815a7f2d4cad325f744a78aceedb1869
nereaMrtz <nerea.martinez@enti.cat>
2025-03-29 19:33:31 +0100

    ForceField Fixed

--- URP-Shaders-Base/Assets/E1/Shaders/SoftLightNode.hlsl
+++ URP-Shaders-Base/Assets/E1/Shaders/SoftLightNode.hlsl
@@ -1,5 +1,8 @@
 void SoftLightNode_float(float fresnel, float intersection, float scanlines, float hexagons, out float Alpha) {
     float base = fresnel + intersection;
     float blend = scanlines * hexagons;
+
+     blend *= saturate(pow(fresnel, 0.7)); // Ajuste para que las lineas de scanline tambien se difuminen con fresnel
+
     Alpha = SoftLight(base, blend).r; 
 }
